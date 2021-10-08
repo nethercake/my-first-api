ac89102b4f95e9eeea9537cf8362565adc4767dc
nethercake <guillaume.esmaeili@gmail.com>
2021-10-09 00:10:51 +0200

    Added Spring boot main class

---
+++ src/main/java/com/example/myfirstapi/MyFirstApi.java
@@ -0,0 +1,13 @@
+package com.example.myfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstApi.class, args);
+    }
+
+}
